a5b89cb23eae387f6d7a4162c9ab038aa2a6bd48
misanc21 <misanc21@gmail.com>
2020-09-21 22:19:56 -0500

    css de crear cuenta

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -49,7 +49,6 @@ const Layout = props => {
                 `}
             />
             <Head>
-                <html lang="es"/>
                 <title>Product hunt clone</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,14 +1,54 @@
+/** @jsx jsx */
 import Layout from '../components/layouts/Layout'
-import styled from '@emotion/styled'
 
-const Heading = styled.h1`
-  color:red;
-`;
+import {Formulario, Campo, Inputsubmit} from '../components/ui/Formulario'
+import {css, jsx} from '@emotion/core'
+
 const CrearCuenta = ()=> {
   return (
     <div>
       <Layout>
-        <Heading>Crear Cuenta</Heading>
+        <>
+          <h2
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >Crear cuenta</h2>
+          <Formulario>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input 
+                type="text" 
+                id="nombre"
+                placeholder="Tu nombre"
+                name="nombre"
+              />
+            </Campo>
+            <Campo>
+              <label htmlFor="email">Email</label>
+              <input 
+                type="email" 
+                id="email"
+                placeholder="Tu email"
+                name="email"
+              />
+            </Campo>
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password" 
+                id="password"
+                placeholder="Tu password"
+                name="password"
+              />
+            </Campo>
+            <Inputsubmit
+              type="submit"
+              value="crear cuenta"
+            />
+          </Formulario>
+        </>
 
       </Layout>
     </div>
