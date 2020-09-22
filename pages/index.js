5c5f5aecd8c93dd4be38bf6ef00aff3eb3b142ea
misanc21 <misanc21@gmail.com>
2020-09-22 18:20:46 -0500

    crear inicio

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,45 @@
+import React, {useEffect, useState, useContext} from 'react'
 import Layout from '../components/layouts/Layout'
-import styled from '@emotion/styled'
+import {FirebaseContext} from '../firebase'
 
-const Heading = styled.h1`
-  color:red;
-`;
+import DetalleProducto from '../components/layouts/DetalleProducto'
+
 export default function Home() {
+  const [productos, setProductos] = useState([])
+  const { firebase } = useContext(FirebaseContext)
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+    }
+    obtenerProductos()
+  }, [])
+
+  const manejarSnapshot = snapshot => {
+    const productos = snapshot.docs.map(doc =>{
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    setProductos(productos)
+  }
+
   return (
     <div>
       <Layout>
-        <Heading>Inicio</Heading>
-
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map(producto =>(
+                <DetalleProducto
+                  key={producto.id}
+                  producto = {producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   )
