77032616b59d3ff8877192c4cf14fa8b8f1242fe
misanc21 <misanc21@gmail.com>
2020-09-21 20:53:14 -0500

    navegacion

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,26 +1,92 @@
+/** @jsx jsx */
 import React from 'react';
+import styled from '@emotion/styled'
+import { css, jsx } from '@emotion/core'
+
+import Boton from '../ui/Boton'
+
 import Buscar from '../ui/Buscar'
 import Navegacion from './Navegacion'
 import Link from 'next/link'
 
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width:768px){
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`
+
 const Header = () => {
-    return ( 
-        <header>
-            <div>
-                <div>
-                    <p>P</p>
-                    <Buscar/>
-                    <Navegacion/>
+    const usuario = true;
+
+    return (
+        <header
+            css={css`
+                    border-bottom: 2px solid var(--gris3);
+                    padding: 1rem 0;
+            `}
+        >
+            <ContenedorHeader>
+                <div
+                    css= {css`
+                        display:flex;
+                        align-items: center;
+                    `}
+                >
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
+                    <Buscar />
+                    <Navegacion />
                 </div>
-                <div>
-                    <p>Hola Misael</p>
-                    <button type="button">Cerrar sesión</button>
-                    <Link href="/">Login</Link>
-                    <Link href="/">Cerrar cuenta</Link>
+                <div css={css`
+                    display: flex;
+                    align-items: center;
+                `}>
+                    {usuario ? 
+                    (
+                        <>
+                        <p
+                        css={css`
+                            margin-right: 2rem;
+                        `}
+                        >
+                            Hola Misael</p>
+                        <Boton
+                            bgColor="true"
+                        >Cerrar sesión</Boton>
+                        </>
+                    ):
+                    (
+                        <>
+                        <Link href="/">
+                            <Boton
+                                bgColor="true"
+                            >Login</Boton>
+                        </Link>
+                        <Link href="/">
+                            <Boton>Crear cuenta</Boton>
+                        </Link>
+                        </>
+                    )}
+                    
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
-     );
+    );
 }
-  
+
 export default Header;
--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -1,10 +1,41 @@
+/** @jsx jsx */
 import React from 'react';
+import styled from '@emotion/styled'
+import { css, jsx } from '@emotion/core'
+
+const InputText = styled.input`
+    border: 1px solid var(--gris3);
+    padding: 1rem;
+    min-width: 300px;
+`
+
+const Inputsubmit =  styled.button`
+    width: 3rem;
+    height: 3rem;
+    display: block;
+    background-size: 4rem;
+    background-image: url('/static/img/buscar.png');
+    position: absolute;
+    right: 1rem;
+    top: 1px;
+    border: none;
+    background-color: white;
+    text-indent: -55em;
+
+    &:hover{
+        cursor: pointer;
+    }
+`
 
 const Buscar = () => {
     return ( 
-        <form>
-            <input type="text"/>
-            <button type="submit">Buscar</button>
+        <form
+            css={css`
+                position: relative;
+            `}
+        >
+            <InputText type="text" placeholder="buscar productos"/>
+            <Inputsubmit type="submit">Buscar</Inputsubmit>
         </form>
      );
 }
